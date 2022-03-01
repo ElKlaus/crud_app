299e112e4a77b6ffee0c85f95b03873936961207
ElKlaus <klaus15rus@gmail.com>
2022-03-01 15:49:17 +0300

    Test app ready

--- js/common.js
+++ js/common.js
@@ -1,1 +1,18 @@
-// $('body').hide()
+$(document).ready(function() {
+  $('button.otpravka').on('click', function() {
+    var titleValue = $('input.title').val();
+    var contentValue = $('textarea.content').val();
+
+    $.ajax({
+      method: "POST",
+      url: "post.php",
+      data: { title: titleValue, content: contentValue }
+    })
+    .done(function() {
+      // alert("Data Daved: " + msg);
+    })
+
+    $('input.title').val('');
+    $('textarea.content').val('');
+  })
+});
